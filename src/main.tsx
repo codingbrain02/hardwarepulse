af4ef5d8625c9031aeb02a60cc4e219b99eff54f
Codingbrain <codingbrain01@gmail.com>
2026-03-16 08:38:17 +0800

    updated app

--- src/main.tsx
+++ src/main.tsx
@@ -5,14 +5,14 @@ import './index.css'
 import App from './App.tsx'
 import PrivacyPolicy from './components/PrivacyPolicy'
 import TermsOfService from './components/TermsOfService'
-//import Window from './components/Window'
+import Window from './components/Window'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
-        {/*<Route path="/" element={<Window />} />*/}
+        <Route path="/HardwarePulse" element={<App />} />
+        <Route path="/" element={<Window />} />
         <Route path="/privacy" element={<PrivacyPolicy />} />
         <Route path="/terms" element={<TermsOfService />} />
       </Routes>
