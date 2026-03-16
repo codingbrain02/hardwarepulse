f6f3156d1b5fbdd9463234943ffabf4a516c7935
Codingbrain <codingbrain01@gmail.com>
2026-03-16 10:09:04 +0800

    updated app

--- src/App.tsx
+++ src/App.tsx
@@ -162,16 +162,6 @@ function App() {
               By downloading, you agree to our Terms of Service and Privacy Policy
             </p>
           </div>
-
-          {/* System Requirements */}
-          <div className="text-left max-w-2xl mx-auto">
-            <button className="text-gray-400 hover:text-sky-400 text-sm flex items-center gap-2 mx-auto">
-              <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
-                <path d="M9 18l6-6-6-6"></path>
-              </svg>
-              System requirements and compatibility
-            </button>
-          </div>
         </div>
 
         {/* Trust Badges */}
